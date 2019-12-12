8486f7013e9386f212e89c2609e5659205828d31
umu+ <aydinumut82@gmail.com>
2019-12-12 11:46:43 +0300

    base class implemented

---
+++ src/base.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const is = require('is_js');
+const pino = require('pino');
+
+/**
+ * @description Base class
+ * @class Base
+ */
+class Base {
+    /**
+     *Creates an instance of Base.
+     * @param {Object} options
+     * @memberof Base
+     */
+    constructor(options) {
+        this.options = Object.assign({}, options || {});
+
+        this.logger = pino(Object.assign({ level: 'error' },
+            is.object(this.options.pino) ? this.options.pino : {}));
+        this.services = {};
+    }
+}
+
+module.exports = Base;
